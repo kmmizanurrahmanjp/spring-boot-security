c49d48f679223c44b9974c5c83f1044cbdfa3204
kmmizanurrahmanjp <https.mizan@gmail.com>
2018-10-29 23:00:29 +0600

    Add database authentication with datasourse

--- script.sql
+++ script.sql
@@ -29,8 +29,18 @@ CREATE TABLE `employees` (
   `employee_phone` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`employee_id`),
   UNIQUE KEY `employee_phone_UNIQUE` (`employee_phone`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (1,'Md. Mizanur Rahman','Dhaka','01725400435'),(4,'Md. Samim Rahman','Dhaka','01725400455'),(5,'Saiful Islam','Dhaka','01719869754');
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `hibernate_sequence`
@@ -43,6 +53,94 @@ CREATE TABLE `hibernate_sequence` (
   `next_val` bigint(20) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+LOCK TABLES `hibernate_sequence` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` VALUES (59);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `persistent_logins`
+--
+
+DROP TABLE IF EXISTS `persistent_logins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `persistent_logins` (
+  `username` varchar(50) NOT NULL,
+  `series` varchar(64) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`series`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `persistent_logins`
+--
+
+LOCK TABLES `persistent_logins` WRITE;
+/*!40000 ALTER TABLE `persistent_logins` DISABLE KEYS */;
+/*!40000 ALTER TABLE `persistent_logins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_roles` (
+  `user_role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_role_id`),
+  UNIQUE KEY `uni_username_role` (`role`,`username`),
+  KEY `fk_username_idx` (`username`),
+  CONSTRAINT `fk_username` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` VALUES (1,'admin','ROLE_ADMIN'),(2,'employee','ROLE_EMP');
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `username` varchar(45) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('admin','$2a$10$gGZHRaz0Y.MS4A4PQgHfLObBThbKJkRScKUcjpo4C/S1UYXsPxvJa',1),('employee','$2a$10$.4HNmaTl/n1Cj8K7SBO01.qemKqS81Sof8zgwv2VZkeWqkinm9U7i',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -53,4 +151,4 @@ CREATE TABLE `hibernate_sequence` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-26 21:09:58
+-- Dump completed on 2018-10-29 22:51:01
--- src/main/java/xyz/mizan/springbootsecurity/boot/config/SpringSecurityConfig.java
+++ src/main/java/xyz/mizan/springbootsecurity/boot/config/SpringSecurityConfig.java
@@ -1,20 +1,102 @@
 package xyz.mizan.springbootsecurity.boot.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @Configuration
 @EnableWebSecurity
+@PropertySource("classpath:application.properties")
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	
+	/*
+	 * 
+	 * Database authentication and persistent token
+	 * 
+	 */
 	@Autowired
+	private Environment env;
+	
+	@Autowired
+    PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	CustomizeAuthenticationSuccessHandler customizeAuthenticationSuccessHandler;
+	
+	@Autowired
+	PersistentTokenRepository tokenRepository;
+	
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+	  auth.jdbcAuthentication().dataSource(dataSource())
+		.usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?")
+		.authoritiesByUsernameQuery("SELECT username, role FROM user_roles WHERE username = ?")
+		.passwordEncoder(encoder());
+	}
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers("/admin/**").hasRole("ADMIN")
+        .antMatchers("/employee/**").hasAnyRole("EMP")
+        .antMatchers("/").permitAll()
+        .and()
+        .formLogin().loginPage("/login").failureUrl("/login?error").usernameParameter("username").passwordParameter("password").successHandler(customizeAuthenticationSuccessHandler)	
+	    .and()
+	    .rememberMe().rememberMeParameter("remember-me").tokenRepository(tokenRepository).tokenValiditySeconds(300)
+        .and()
+		.logout().logoutSuccessUrl("/logout")
+		.and()
+        .exceptionHandling().accessDeniedPage("/access-denied")
+        .and()
+        .csrf();
+        
+    }
+	
+	@Bean
+	public DataSource dataSource() {
+		return DataSourceBuilder
+				.create()
+				.username(env.getProperty("spring.datasource.username"))
+				.password(env.getProperty("spring.datasource.password"))
+				.url(env.getProperty("spring.datasource.url"))
+				.driverClassName(env.getProperty("com.mysql.jdbc.Driver"))
+				.build();
+	}
+	
+	@Bean
+	  public PersistentTokenRepository tokenRepository() {
+	    JdbcTokenRepositoryImpl jdbcTokenRepositoryImpl=new JdbcTokenRepositoryImpl();
+	    jdbcTokenRepositoryImpl.setDataSource(dataSource());
+	    return jdbcTokenRepositoryImpl;
+	}
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	/*
+	 * 
+	 * Inmamory authentication hash token
+	 * 
+	 */
+	/*@Autowired
     PasswordEncoder passwordEncoder;
 	
 	@Autowired
@@ -46,9 +128,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
         
     }
 	
-	
 	@Bean
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
-	}
+	}*/
 }
